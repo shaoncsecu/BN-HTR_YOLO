d837404df6df60d49f9a930b4db408b44ea658a9
MD ATAUR RAHMAN <ataur@essi.upc.edu>
2023-04-27 03:18:25 +0200

    Pushing the Codes

---
+++ app.py
@@ -0,0 +1,165 @@
+import glob
+import streamlit as st
+import wget
+from PIL import Image
+import torch
+import cv2
+import os
+import time
+
+st.set_page_config(layout="wide")
+
+cfg_model_path = 'models/line_model_best.pt'
+model = None
+confidence = .25
+
+
+def image_input(data_src):
+    img_file = None
+    if data_src == 'Sample data':
+        # get all sample images
+        img_path = glob.glob('data/sample_documents/*')
+        img_slider = st.slider("Select a test image.", min_value=1, max_value=len(img_path), step=1)
+        img_file = img_path[img_slider - 1]
+    else:
+        img_bytes = st.sidebar.file_uploader("Upload an image", type=['png', 'jpeg', 'jpg'])
+        if img_bytes:
+            img_file = "data/uploaded_data/upload." + img_bytes.name.split('.')[-1]
+            Image.open(img_bytes).save(img_file)
+
+    if img_file:
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(img_file, caption="Selected Image")
+        with col2:
+            img = infer_image(img_file)
+            st.image(img, caption="Model prediction")
+
+
+def line_input(data_src):
+    img_file = None
+    if data_src == 'Sample data':
+        # get all sample images
+        img_path = glob.glob('data/sample_lines/*')
+        img_slider = st.slider("Select a test image.", min_value=1, max_value=len(img_path), step=1)
+        img_file = img_path[img_slider - 1]
+    else:
+        img_bytes = st.sidebar.file_uploader("Upload an image", type=['png', 'jpeg', 'jpg'])
+        if img_bytes:
+            img_file = "data/uploaded_data/upload." + img_bytes.name.split('.')[-1]
+            Image.open(img_bytes).save(img_file)
+
+    if img_file:
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(img_file, caption="Selected Image")
+        with col2:
+            img = infer_image(img_file)
+            st.image(img, caption="Model prediction")
+
+
+def infer_image(img, size=None):
+    model.conf = confidence
+    result = model(img, size=size) if size else model(img)
+    result.render()
+    image = Image.fromarray(result.ims[0])
+    return image
+
+
+@st.cache_resource
+def load_model(path, device):
+    model_ = torch.hub.load('ultralytics/yolov5', 'custom', path=path, force_reload=True)
+    model_.to(device)
+    print("model to ", device)
+    return model_
+
+
+@st.cache_resource
+def download_model(url):
+    model_file = wget.download(url, out="models")
+    return model_file
+
+
+def get_user_model():
+    model_src = st.sidebar.radio("Model source", ["file upload", "url"])
+    model_file = None
+    if model_src == "file upload":
+        model_bytes = st.sidebar.file_uploader("Upload a model file", type=['pt'])
+        if model_bytes:
+            model_file = "models/uploaded_" + model_bytes.name
+            with open(model_file, 'wb') as out:
+                out.write(model_bytes.read())
+    else:
+        url = st.sidebar.text_input("model url")
+        if url:
+            model_file_ = download_model(url)
+            if model_file_.split(".")[-1] == "pt":
+                model_file = model_file_
+
+    return model_file
+
+def main():
+    # global variables
+    global model, confidence, cfg_model_path
+
+    st.title("BN-DRISHTI Line/Word Segmentation using only YOLO Model")
+
+    st.sidebar.title("Settings")
+
+    # upload model
+    model_src = st.sidebar.radio("Select yolov5 weight file", ["Line Segmentation Model", "Word Segmentation Model"])
+    # URL, upload file (max 200 mb)
+    if model_src == "Word Segmentation Model":
+        #user_model_path = get_user_model()
+        user_model_path = 'models/word_model_best.pt'
+        if user_model_path:
+            cfg_model_path = user_model_path
+
+        st.sidebar.text(cfg_model_path.split("/")[-1])
+        st.sidebar.markdown("---")
+
+
+    # check if model file is available
+    if not os.path.isfile(cfg_model_path):
+        st.warning("Model file not available!!!, please added to the model folder.", icon="⚠️")
+    else:
+        # device options
+        if torch.cuda.is_available():
+            device_option = st.sidebar.radio("Select Device", ['cpu', 'cuda'], disabled=False, index=0)
+        else:
+            device_option = st.sidebar.radio("Select Device", ['cpu', 'cuda'], disabled=True, index=0)
+
+        # load model
+        model = load_model(cfg_model_path, device_option)
+
+        # confidence slider
+        confidence = st.sidebar.slider('Confidence', min_value=0.1, max_value=1.0, value=.45)
+
+        # custom classes
+        if st.sidebar.checkbox("Custom Classes"):
+            model_names = list(model.names.values())
+            assigned_class = st.sidebar.multiselect("Select Classes", model_names, default=[model_names[0]])
+            classes = [model_names.index(name) for name in assigned_class]
+            model.classes = classes
+        else:
+            model.classes = list(model.names.keys())
+
+        st.sidebar.markdown("---")
+
+        # input options
+        input_option = st.sidebar.radio("Select input type: ", ['Documents', 'Lines'])
+
+        # input src option
+        data_src = st.sidebar.radio("Select input source: ", ['Sample data', 'Upload your own data'])
+
+        if input_option == 'Documents':
+            image_input(data_src)
+        else:
+            line_input(data_src)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit:
+        pass
